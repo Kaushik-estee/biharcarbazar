0bb1eef8be8c280958ab33e2ddf3ef18c008b3d5
kaushik.estee <kaushik.estee@gmail.com>
2023-10-11 14:45:29 +0530

    ll

---
+++ my-app/src/Ui/Components/Productcard2.jsx
@@ -0,0 +1,167 @@
+// product card
+import {
+    Box,
+    Center,
+    useColorModeValue,
+    Heading,
+    Text,
+    Stack,
+    Image,
+    Flex,
+  
+  } from '@chakra-ui/react';
+import { color } from 'framer-motion';
+  import {Link as RouterLink} from "react-router-dom"
+import WhatsAppButton from './Whatsapp';
+import PhoneCallButton from './Call';
+import "./ProductCard.css"
+  
+  // const IMAGE =
+  //   'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
+  
+  export default function ProductCard({year,id,make,price,image,model,kms,transmission,exteriorcolor,fuel,booked}) {
+    const handleCardClick = () => {
+        // Navigate to the product details page with the product ID as a URL parameter
+        // You should replace '/product-details' with the actual path to your product details page
+      
+      window.location.href = `/${id}`;
+      };
+    return (
+      <Center py={12}>
+        
+   
+        <Box
+        onClick={handleCardClick}
+        roundedBottom={'xl'}
+          role={'group'}
+          p={4}
+          maxW={'450px'}
+          w={'full'}
+          bg={useColorModeValue('white', 'gray.800')}
+          // boxShadow={'2xl'}
+         
+          pos={'relative'}
+          zIndex={1}
+      
+          
+          >
+                 <RouterLink to={`/${id}`}>
+          <Box
+            roundedTop={'xl'}
+            mt={-12}
+            backgroundColor={'#EDF0F9'}
+            pos={'relative'}
+            height={'230px'}
+          
+            _groupHover={{
+              _after: {
+                filter: 'blur(20px)',
+              },
+            }}>
+                   {booked === 'yes' ? (
+            <>
+            
+              <button style= {{width:'80px',height:'30px',zIndex:'2000',position:'absolute',background:'red',color:'white',marginTop:'8px',borderBottomRightRadius:'11px',borderTopRightRadius:'11px'}}>BOOKED</button>
+            </>
+          ) : booked === 'sold'?  (
+            <>
+            
+              <button style= {{width:'80px',height:'30px',zIndex:'2000',position:'absolute',background:'green',color:'white',marginTop:'8px',borderBottomRightRadius:'11px',borderTopRightRadius:'11px'}}>SOLD</button>
+            </>
+          ):null}
+            <Image
+              rounded={'xl'}
+              height={"100%"}
+              width={"100%"}
+              objectFit={'fit'}
+              src={image}
+              position={'absolute'}
+            />
+            {/* style={{position:'absolute',zIndex:'3000',width:'100%',marginTop:'62%',justifyContent:'space-between'}} */}
+             {/* <div id='callIcons'  ></div> */}
+              <h1 id='whatsappIcon'> <WhatsAppButton/></h1>
+              <h1 id='calIcon'><PhoneCallButton/></h1>
+           
+            
+          
+          </Box>
+          </RouterLink>
+          <div style={{background:"#EDF0F9",borderBottomLeftRadius:"8px",borderBottomRightRadius:"8px"}} >
+          <Stack >
+          <div style={{background:"#EDF0F9",width:"90%",margin:"auto"}}>
+          <RouterLink to={`/${id}`}>
+       
+          <Heading   color={'#27005D'}  marginTop={'25px'} fontSize={"x-large"} > 
+              {year} {make} {model} {fuel} 
+
+              </Heading>
+              </RouterLink>
+              </div>
+              <div style={{width:'90%',margin:"auto"}}>
+              <Text textDecoration={'none'}  color={'#27005D'} fontWeight={500} fontSize={'xl'}> PRICE  
+              &#8377; {price}
+              </Text>
+              </div>
+              <RouterLink to={`/${id}`}>
+            
+              <div style={{display:'flex',width:'100%',justifyContent:'space-around'}}>
+              
+            <div style={{width:'30%',margin:"11px",textAlign:'center',display: 'flex',borderRight:'1px solid #D4C5C7',
+  flexDirection: 'column',
+  justifyContent: 'center', // Center content vertically
+  alignItems: 'center',
+  marginBottom:'20px'}} >
+             {/* <img style={{margin:'5px',width:'18px',height:'18px'}} src="https://cdn.bigboytoyz.com/new-version/attributes/calender1-icon.png" alt="" /> */}
+             <h6 id='regYear' style={{color:'#A39BA8',fontWeight:'bold',marginTop:"7px"}}>FUEL TYPE</h6>
+            <Text marginTop={'2px'}  textDecoration={'none'} color={'#27005D'} fontSize={'sm'} fontWeight={'500'}>
+              {fuel} 
+            </Text>
+            </div>
+              
+              <div style={{width:'30%',margin:"10px",textAlign:'center',borderRight:'1px solid #D4C5C7',display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center', // Center content vertically
+  alignItems: 'center',
+  marginBottom:'20px'}}>
+           <div>
+              <h6 id='km' style={{color:'#A39BA8',fontWeight:'bold',marginTop:"7px"}}>KM</h6>
+              <Text  color={'#27005D'} fontWeight={'500'} fontSize={'sm'}   > 
+              {kms}
+              </Text>
+              </div>
+              </div>
+              <div style={{width:'30%',margin:"10px",textAlign:'center',display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center', // Center content vertically
+  alignItems: 'center',
+  marginBottom:'20px'}}>
+              {/* <img style={{margin:'5px',width:'18px',height:'18px'}} src="https://cdn.bigboytoyz.com/new-version/attributes/kms-done.png" alt="" /> */}
+              <h6 style={{color:'#A39BA8',fontWeight:"bold",marginTop:"7px"}}>TRANSMISSION</h6>
+              <Text  color={'#27005D'} fontWeight={'500'} fontSize={'sm'}  > 
+              {transmission}
+              </Text>
+              </div>
+           
+              
+            
+             
+             
+              
+              
+              </div>
+            
+            </RouterLink>
+             
+              
+          </Stack>
+         
+          </div>
+         
+       
+          
+        </Box>
+
+  
+      </Center>
+    );
+  }
---
+++ my-app/src/Ui/Components/Productdetails4.jsx
@@ -0,0 +1,248 @@
+import { useState,useEffect } from 'react';
+import {  Text,Heading,Image, GridItem, Grid
+    } from "@chakra-ui/react"
+
+import axios from 'axios';
+import { FiPhoneCall } from 'react-icons/fi';
+import './Productdetails.css'
+
+  import {useParams} from "react-router-dom"
+import Footer from './Footer';
+import Nav from '../Nav';
+import ImageCarousel from './Image';
+import Loading from './Loading';
+import PhoneCallButton from './Call';
+import PhoneCallButton2 from './Callbutton';
+import ProductCard from './Productcard2';
+
+  export default function ProductDetails() {
+    const [product,setProduct] = useState({})
+    const [relproduct,setRelproduct] = useState([])
+    const[relative,setRelative] = useState({gte:0,lte:0})
+    const [isLoading, setIsLoading] = useState(true);
+
+   
+    const {id}  = useParams()
+  
+    
+console.log("id",id);
+useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`https://vast-pear-nightingale-sari.cyclic.app/cars/${id}`);
+            const data = response.data;
+            console.log("dataaaa",data)
+            if (data) {
+                const fetchedProduct = data;
+               
+                setProduct(fetchedProduct);
+                setIsLoading(false);
+
+                // Calculate relative values here
+                const pp = fetchedProduct.price;
+                const newRelative = { gte: pp - 200000, lte: pp + 200000 };
+                setRelative(newRelative);
+
+                // Call fetchdata2 with the newRelative values
+                fetchdata2(newRelative);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    fetchData();
+}, [id]);
+
+
+const fetchdata2 = async (newRelative) => {
+    try {
+        const response = await axios.get(`https://vast-pear-nightingale-sari.cyclic.app/cars?minPrice=${newRelative.gte}&maxPrice=${newRelative.lte}`);
+        const responseData = response.data;
+        if (responseData.cars.length > 1) {
+            setRelproduct(responseData.cars);
+        } else {
+            document.getElementById('showRelative').textContent = 'No Product To show';
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+  
+
+
+
+const {year,kms,make,price,model,transmission,exteriorcolor,fuel,_id}=product
+
+
+const images = [
+  product.image,
+  product.image2,
+  product.image3,
+  product.image4,
+  product.image5,
+  product.image6
+  
+ 
+  // Add more image URLs as needed
+];
+
+const filteredImages = images.filter(image => image);
+    
+    
+    
+
+
+    return (
+        <div>
+          
+     
+   <div width={'100%'}  >
+    {/* navbar---- */}
+    <Nav/>
+    {/* <div style={{width:'98%',margin:'auto',marginTop:'10px'}} >
+   
+    </div> */}
+    <div style={{height:'100px'}}></div>
+    {isLoading ? (
+<Loading/>
+              ):(
+   
+    <div id='detail' width={'98%'} margin={'auto'}   >
+    <div id='main' style={{minHeight:'600px'}}  >
+      <div  id='prodImage' >
+      <ImageCarousel  images={filteredImages} />
+        
+{/* <Image id='prodImg'  src={product.image} alt={kms} /> */}
+      </div>
+      
+      <div     id='proDetail' >
+        <h1 style={{fontWeight:'bold',color:'#F26522'}}>Details</h1>
+        <Heading color={'#27005D'} > {make}</Heading>
+        <Text    style={{fontWeight:'bold',color:'#27005D'}}>  &#8377; {price}</Text>
+        <div style={{display:'flex'}}>
+             <img style={{margin:'5px',width:'18px',height:'18px'}} src="https://cdn.bigboytoyz.com/new-version/attributes/calender1-icon.png" alt="" />
+            <Text color={'#27005D'} fontWeight={'500'}  >
+              {year}
+            </Text>
+            </div>
+        {/* <Text  >YEAR: {year}</Text> */}
+        <div style={{display:'flex',marginTop:'5px'}}>
+              <img style={{margin:'5px',width:'18px',height:'18px'}} src="https://cdn.bigboytoyz.com/new-version/attributes/kms-done.png" alt="" />
+              <Text  color={'#27005D'} fontWeight={'500'}  > 
+              {kms}
+              </Text>
+              </div>
+         
+        
+          <div style={{display:'flex',marginTop:'5px'}}>
+              <img style={{margin:'5px',width:'18px',height:'18px'}} 
+              src="https://cdn.bigboytoyz.com/new-version/attributes/setting-icon.png" alt="" /> 
+              <Text color={'#27005D'} fontWeight={'500'}  > 
+                {transmission}
+              </Text>
+              </div>
+        
+        {/* <Text    marginLeft="5px" id="price2" > TRANSMISSION: {transmission}</Text> */}
+        
+        {/* <Text   marginLeft="5px" id="price2" > MODEL: {model}</Text> */}
+        <div style={{display:'flex'}}>
+              <img style={{margin:'5px',width:'23px',height:'18px'}} src="https://cdn.bigboytoyz.com/new-version/attributes/cartype-icon.png" alt="" />
+              <Text margin={'5px'} color={'#27005D'} fontWeight={'500'} > 
+               {model}
+              </Text>
+              </div>
+       
+        {/* <Text    marginLeft="5px" id="price2" >COLOR: {exteriorcolor}</Text> */}
+        <div style={{display:'flex'}}>
+              <img style={{margin:'5px',width:'20px',height:'20px'}}  src="https://cdn.bigboytoyz.com/new-version/attributes/exterior-color.png" alt="" />
+              <Text  margin={'5px'}  color={'#27005D'} fontWeight={'500'}  > 
+              {exteriorcolor}
+              </Text>
+              </div>
+        
+        {/* <Text   marginLeft="5px" id="price2" >FUEL: {fuel}</Text> */}
+        <div style={{display:'flex'}}>
+              <img style={{margin:'5px',width:'20px',height:'20px'}} src="https://cdn.bigboytoyz.com/new-version/attributes/fueltype-icon.png" alt="" />
+              <Text  color={'#27005D'} fontWeight={'500'} > 
+               {fuel}
+              </Text>
+              </div>
+        <div style={{justifyContent:'space-around'}}>
+          {/* <div id='numshown'>
+            <h1 style={{fontWeight:'600',color:'#27005D'}}>Call Now : 9297777103</h1>
+          </div >
+          <div id='call2'> <PhoneCallButton2/></div> */}
+       
+        
+         
+       
+          </div>
+        
+        
+        
+      </div>
+      </div>
+      <div>
+
+
+      </div>
+    </div>
+              )}
+   
+{/* footer---------------------- */}
+
+
+   </div>
+   <div>
+  
+
+   </div>
+   <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+    {/* <button id='showRelative' style={{background:'orange',color:"white",margin:'auto',width:"180px",height:'50px',borderRadius:"8px",marginBottom:'5px'}} onClick={fetchdata2}>SHOW RELATIVE PRODUCTS</button>
+    */}
+   </div>
+   <div>
+   <div style={{width:'90%',margin:'auto'}}>
+              {isLoading ? (
+<Loading/>
+              ):(<Grid  templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(2, 1fr)','repeat(3, 1fr)']} gap={6}> 
+              {/* mapping product */}
+          {
+              relproduct?.length>0 && relproduct.map((e)=>{
+          return (
+          <GridItem key={e.id} >
+              {/* mapping in card */}
+          <ProductCard
+          
+          image={e.image}
+          id={e._id}
+          year={e.year}
+          price={e.price}
+          
+          make={e.make}
+          
+          model = {e.model}
+          kms = {e.kms}
+          transmission={e.transmission}
+          fuel={e.fuel}
+          exteriorcolor={e.exteriorcolor}
+          booked={e.booked}
+         
+          
+          />
+          </GridItem>
+               ) })
+          }
+          
+          </Grid>)}
+          
+          
+
+
+</div>
+   </div>
+    <Footer/> 
+   </div>
+    );
+  }
